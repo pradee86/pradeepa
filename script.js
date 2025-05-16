49be247dc9e5db1043f04ba8826e3effbb61415c
pradee <pradeepausha2005@gmail.com>
2025-05-16 16:03:07 +0530

    second

---
+++ script.js
@@ -0,0 +1,25 @@
+var a=70;
+var b=55;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a>b)
+console.log(a<b)
+console.log(a=b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a>>b)
+function add(){
+    var a=70;
+    var b=55;
+    console.log(a+b)
+}
+add()
+function add(a,b){
+    console.log(a+b)
+}
+add(30,20)
+add(40,60)
+add(70,55)
